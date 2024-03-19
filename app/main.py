233f70d1f31f726a343be79b585441d0297abd3d
Adam Carroll <Oceloteater@users.noreply.github.com>
2024-03-19 12:17:39 +0200

    Committing create and get endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Path, Depends
 from typing import Union
+from datetime import datetime
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 from app.tasks import scheduler, schedule_tasks
 from app.models import Announcement, Base
@@ -17,17 +18,17 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
 
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
 def get_db():
     db = SessionLocal()
-    return db  # Return the session object directly
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def check_db():
+    db = SessionLocal()
+    return db
 
 
 app = FastAPI()
@@ -37,15 +38,43 @@ app = FastAPI()
 def startup_event():
     schedule_tasks()
     scheduler.start()
-    with get_db() as db:
-        print('Db connected successfully')
+    with check_db() as db:
+        print("Db connected successfully")
 
     print(f"Scheduler started: {scheduler}")
 
 
 @app.get("/")
 async def root():
-    return {"message": "FastAPI app with background task!"}
+    return {"message": "Health check OK", "status": 200}
+
+
+@app.post("/api/v1/announcements")
+async def schedule_announcement(message: str = Body(...), send_at: datetime = Body(...), db: Session = Depends(get_db)):
+    print(f"Announcement scheduled: {message} - Scheduled for: {send_at}")
+
+    announcement = Announcement(message=message, send_at=send_at)
+    db.add(announcement)
+    db.commit()
+    db.refresh(announcement)
+
+    return {"message": f"Announcement scheduled successfully. ID: {announcement.id}"}
+
+
+@app.get("/api/v1/announcements/{announcement_id}")
+async def get_announcement_by_id(announcement_id: int = Path(..., description="ID of the announcement to retrieve"), db: Session = Depends(get_db)):
+
+    announcement = db.query(Announcement).filter(Announcement.id == announcement_id).first()
+
+    if not announcement:
+        return {"message": f"Announcement with ID {announcement_id} not found"}
+
+    return {
+        "id": announcement.id,
+        "message": announcement.message,
+        "status": announcement.status.value,
+        "send_at": announcement.send_at.isoformat() if announcement.send_at else None
+    }
 
 
 @app.get("/items/{item_id}")
